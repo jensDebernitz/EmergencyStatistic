4e0f5feee4406e490bc951d09b4393af09ce003f
Jens Debernitz <jens.debernitz@cgm.com>
2024-07-29 17:07:49 +0200

    added more

--- ExcelTabellenAuswerung/App.xaml.cs
+++ ExcelTabellenAuswerung/App.xaml.cs
@@ -14,17 +14,7 @@ namespace ExcelTabellenAuswerung
     /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-            //SettingsDataBase settingsDataBase = new SettingsDataBase();
-
-            //BaseTheme baseTheme = settingsDataBase.LoadTheme();
-            //Color primaryColor = SwatchHelper.Lookup[MaterialDesignColor.DeepPurple];
-            //Color secondaryColor = SwatchHelper.Lookup[MaterialDesignColor.Lime];
-            //Theme theme = Theme.Create(baseTheme, primaryColor, secondaryColor);
-            //Resources.SetTheme(theme);
-            
-        }
+      
     }
 
 }
---
+++ ExcelTabellenAuswerung/Program.cs
@@ -0,0 +1,61 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Environment;
+using System.Windows;
+using Velopack;
+
+namespace ExcelTabellenAuswerung
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            try
+            {
+
+                var commonpath = GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+                var path = Path.Combine(commonpath, "ExcelTabellenAuswertung\\logs");
+
+                if (Directory.Exists(path) == false)
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                Helpers.DataBase.GlobalLogging = new StringWriter();
+
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .WriteTo.Console()
+                    .WriteTo.TextWriter(Helpers.DataBase.GlobalLogging)
+                    .WriteTo.File(path + "\\app.log", rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
+
+                Log.Information("Anwendung gestartet");
+
+
+                //// It's important to Run() the VelopackApp as early as possible in app startup.
+                VelopackApp.Build().Run();
+
+                // We can now launch the WPF application as normal.
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unhandled exception: " + ex.ToString());
+            }
+
+            Log.Information("Anwendung wird beendet");
+            Log.CloseAndFlush();
+        }
+    }
+}
+
